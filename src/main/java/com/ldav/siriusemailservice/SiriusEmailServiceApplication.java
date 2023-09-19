b94d0938df30c6457245d95abe2d3eaf77b000ac
Lucas David Cardozo <lucas.david.car99@gmail.com>
2023-09-19 13:55:36 -0300

    Initial commit

---
+++ src/main/java/com/ldav/siriusemailservice/SiriusEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ldav.siriusemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiriusEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiriusEmailServiceApplication.class, args);
+    }
+
+}
